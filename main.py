bf6107bad3994c20f7a4109154eaddb3f98be589
BLiYing <872904178@qq.com>
2020-07-09 17:08:10 +0800

    每天下午

--- main.py
+++ main.py
@@ -2,8 +2,4 @@
 
 from scrapy import cmdline
 #定时执行任务
-cmdline.execute('scrapy crawl beike_1'.split())
-cmdline.execute('scrapy crawl beike_2'.split())
-cmdline.execute('scrapy crawl beike_3'.split())
-cmdline.execute('scrapy crawl beike_4'.split())
-cmdline.execute('scrapy crawl beike_5'.split())
+cmdline.execute('scrapy crawlall'.split())
